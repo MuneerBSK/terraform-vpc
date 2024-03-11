66494dc80580071ce2014e085966f461e5c56fee
MuneerBSK <muneerbsk96@gmail.com>
2024-03-10 21:08:39 -0400

    AutoPush

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
 module "vpc" {
   source                   = "git::https://github.com/MuneerBSK/tf-module-vpc.git"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
 }
-
