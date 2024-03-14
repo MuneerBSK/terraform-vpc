5220a18301fd8497923bf496b601405dac2270cf
MuneerBSK <muneerbsk96@gmail.com>
2024-03-14 11:02:09 -0400

    AutoPush

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {  
   source      = "./vendor/modules/vpc"
-  VPC_CIDR    = var.CIDR_VPC  # Assuming the module expects a reference to the created VPC
+  CIDR_VPC    = var.CIDR_VPC  # Assuming the module expects a reference to the created VPC
   
   
   }
