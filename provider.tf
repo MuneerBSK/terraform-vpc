91e2f980a863bdf9508deeb1d2f94c8377947af0
MuneerBSK <muneerbsk96@gmail.com>
2024-04-21 15:08:29 -0400

    AutoPush

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
-provider "aws" {
-    region  =   "us-east-1"
-}
+provider "aws" {}
 
-# Values with in the backend config are supplied from the environment specific
 terraform {
   backend "s3" {}
 }
